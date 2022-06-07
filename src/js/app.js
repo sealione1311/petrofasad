389737672fb1b3dce0e13647f4a15e5712e5aa84
Tatyana Mirzova <44976901+sealione1311@users.noreply.github.com>
2022-06-07 19:38:28 +0300

    new version

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,22 @@
 import * as flsFunction from './modules/functions.js'
 
 flsFunction.isWebp()
+
+const swiper = new Swiper('.swiper', {
+  speed: 400,
+
+  slidesPerView: 2,
+  centeredSlides: true,
+  loop: true,
+  breakpoints: {
+    // when window width is >= 320px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    640: {
+      slidesPerView: 4,
+      spaceBetween: 40
+    }},
+
+});
